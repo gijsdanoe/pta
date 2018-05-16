d72f6b1b174f22cf2cfb01903ee878dc3051054c
lennartschepers <lenschepers@gmail.com>
2018-05-16 12:13:27 +0200

    added base week4

---
+++ week4/exercise1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+from nltk import pos_tag, word_tokenize
+
+
+def main():
+    for folder in os.listdir("/home/lennart/projectta/week5/data/"):
+        for folder2 in os.listdir("/home/lennart/projectta/week5/data/" + folder):
+            path = "/home/lennart/projectta/week5/data/" + folder + "/" + folder2 + "/"
+            posfile = open(path + "en.tok.off.pos", "w+")
+            with open(path + "en.tok.off", "r") as myfile:
+                for line in myfile:
+                    columns = line.split()
+                    if len(columns) > 1:
+						columns.append(pos_tag(word_tokenize(columns[3]))[0][1])
+                        posfile.write(" ".join(columns))
+                        posfile.write("\n")
+
+if __name__ == "__main__":
+    main()
