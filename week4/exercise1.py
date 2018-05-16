b2b181d0b9da0b15cf3b47686da247c6e04bdb3b
thomastan1998 <t.c.tan@student.rug.nl>
2018-05-16 12:22:30 +0200

    it's all

--- week4/exercise1.py
+++ week4/exercise1.py
@@ -5,15 +5,15 @@ from nltk import pos_tag, word_tokenize
 
 
 def main():
-    for folder in os.listdir("/home/thomas/projectta/week4/data/"):
-        for folder2 in os.listdir("/home/thomas/projectta/week4/data/" + folder):
-            path = "/home/thomas/projectta/week4/data/" + folder + "/" + folder2 + "/"
+    for folder in os.listdir("/home/thomas/projecttextanalyse/week4/data/"):
+        for folder2 in os.listdir("/home/thomas/projecttextanalyse/week4/data/" + folder):
+            path = "/home/thomas/projecttextanalyse/week4/data/" + folder + "/" + folder2 + "/"
             posfile = open(path + "en.tok.off.pos", "w+")
             with open(path + "en.tok.off", "r") as myfile:
                 for line in myfile:
                     columns = line.split()
                     if len(columns) > 1:
-                                                columns.append(pos_tag(word_tokenize(columns[3]))[0][1])
+                        columns.append(pos_tag(word_tokenize(columns[3]))[0][1])
                         posfile.write(" ".join(columns))
                         posfile.write("\n")
 
