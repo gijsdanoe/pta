02821e60146d143c57a7c96020bdaf6fdfd5058e
lennartschepers <lenschepers@gmail.com>
2018-06-12 17:36:17 +0200

    added eindproject with nertagger functionality

---
+++ eindproject/project.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+#filename: exercise1.py
+#exercise 1 of week 5
+
+import nltk
+from nltk.wsd import lesk 
+from nltk.corpus import wordnet
+import wikipedia
+from nltk.tag import StanfordNERTagger
+import os
+
+
+def main(): 
+    wiki = wikipedia.page("emmen, netherlands")
+    raw = wiki.content
+    sents = nltk.sent_tokenize(raw)
+    words = nltk.word_tokenize(raw)
+    tagged = nltk.pos_tag(words)
+    nouns = [word for word,pos in tagged if pos.startswith('N')]
+    st = StanfordNERTagger('/home/lennart/Downloads/stanford-ner-2018-02-27/classifiers/english.conll.4class.distsim.crf.ser.gz', '/home/lennart/Downloads/stanford-ner-2018-02-27/stanford-ner.jar')
+    #for folder in os.listdir("/home/lennart/projecttextanalyse/eindproject/testdir/"):
+        #for folder2 in os.listdir("/home/lennart/projecttextanalyse/eindproject/testdir/" + folder):
+    path = "/home/lennart/projecttextanalyse/eindproject/testdir/p05/d0580/"
+    testfile = open(path + "en.tok.off.pos.test", "w+")
+    #rawfile = open(path + "en.raw", "r")
+    rawlist = []
+    with open(path + "en.tok.off.pos", "r") as posfile:
+        f = posfile.readlines()
+        for line in f:
+            rawlist.append(line.split()[3])
+
+        rawstring = " ".join(rawlist)
+        l = st.tag(rawstring.split())
+    with open(path + "en.tok.off.pos", "r") as postfile:
+        n = 0
+        for row in postfile:
+            if l[n][1] != "O": 
+                columns = row.split()
+                columns.append(l[n][1])
+                testfile.write(" ".join(columns))
+                testfile.write("\n")
+                n += 1
+            else:
+                columns = row.split()
+                testfile.write(" ".join(columns))
+                testfile.write("\n")
+                n += 1
+
+
+        
+
+
+
+main()
