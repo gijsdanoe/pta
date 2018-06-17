b5a1acf4db907d761483304363fe1f9d459d7c90
Gijs Danoe <gijs-danoe5@hotmail.com>
2018-06-17 23:28:59 +0200

    fvev

--- wordnet.py
+++ wordnet.py
@@ -5,7 +5,7 @@ from nltk.corpus import wordnet as wn
 
 def main():
 #open and read the file, make a list of all nouns
-    path = "/home/gijs/projecttextanalyse/eindproject/testdir/p52/d0116"
+    path = "/home/gijs/projecttextanalyse/eindproject/testdir/p36/d0500"
     testfile = open(path + "/en.tok.off.pos.test", "w+")
     rawlist = []
     with open(path + "/en.tok.off.pos", "r") as posfile:
@@ -17,16 +17,77 @@ def main():
                 pass
         rawstring = " ".join(rawlist)
 
-#make a list of every wordnet definition of every word in the raw text list
+#make a list of every wordnet definition of every word in the raw text list, in case of ambiguity the program calculates the path similarity of every option with every word in the text; he picks the option with the highest cumulative path similarity
     synlist = []
+    ll = []
+    onesynlist = []
+    simlist = []
+    
+    for p in rawlist:
+        onedeflist = []
+        deflist = []   
+        klist = []
+        for syns in wn.synsets(p):
+            deflist.append(syns.definition())
+            onedeflist.append(syns)
+        if not deflist:
+            synlist.append([])
+        elif len(deflist) == 1:
+            synlist.append(deflist[0])
+            onesynlist.append(onedeflist[0])
+        else:
+            klist.append(wn.synsets(p))
+            synlist.append('MLT')
+        for item in klist:
+
+            simlist.append(item)
+        newlist = []
+        for item in simlist:
+            itemb = []
+            for i in item:     
+                synb = []         
+                for a in onesynlist:
+                    ps = i.path_similarity(a)
+                    synb.append(ps)
+                itemb.append(synb)
+            newlist.append(itemb)
+        newlist = [[[0 if x is None else x for x in i] for i in item] for item in newlist]
+        newlist = [[sum(i) for i in item] for item in newlist]
+                     
+    
+    maxlist = []
+    newsynlist = []
+
+    for item in newlist:
+        maxlist.append(item.index(max(item)))
+    for i,value in zip(simlist, maxlist):
+        a = i[value].definition()
+        newsynlist.append(a)
+    newdeflist = []
     for p in rawlist:
         deflist = []
         for syns in wn.synsets(p):
             deflist.append(syns.definition())
-        if not deflist:
-            synlist.append([])
+                   
+            if not deflist:
+                newdeflist.append([])
+            elif len(deflist) == 1:
+                newdeflist.append([])
+            else:
+                for i in range(len(newsynlist)-1):                    
+                    newdeflist.append(newsynlist[0])
+                
+                    del newsynlist[0]
+
+    for (i,syn), (a,x) in zip(enumerate(synlist), enumerate(newdeflist)):
+       
+        if syn == 'MLT':
+                synlist[i] = newdeflist[a]
+                
         else:
-            synlist.append(deflist[0])
+            pass
+     
+
 
 #make a list of all bigrams with nltk, then make a list with every wordnet definition of every bigram
     bigramdeflist = []
@@ -42,13 +103,13 @@ def main():
 
 #lists of all words that could appear in the definitions of the unigrams and bigrams
     city = [' city ', ' village ', ' town ', 'capital']
-    country = [' nation ', ' republic ', ' monarchy ']
-    sport = [' sport ', 'combat', ' game ', 'fight']
+    country = [' nation ', ' republic ', ' monarchy ', ' state ']
+    sport = [' sport ', 'combat', ' game ']
     natural_places = [' desert ', ' volcano ', ' sea ', ' ocean ',  ' lake ', ' river ', ' jungle ', ' waterfall ', ' glacier ', ' mountain ', ' forest ' , ' crater ', ' cave ', ' canyon ', ' fjord ', ' park ', ' bay ', ' valley ', ' cliff ', ' reef ']
     entertainment = [' book ', 'magazine', 'film', 'movie', 'song', 'journal', 'newspaper', 'show', 'band']
     animal = ['mammal', 'bird', 'fish', 'amphibian', 'reptil', 'crustacean', 'insect', 'carnivore', 'herbivore', 'species', 'breed', 'cattle', 'quadruped', 'pachyderm', 'feline', 'ungulate']
     person = ['born']
-    organization = ['company', 'organization', 'charity', 'movement', 'party', 'enterprise', 'business', 'union']
+    organization = ['company', 'organization', 'charity', 'party', 'enterprise', 'business', 'union']
     
 #if one of the words appears in the definition of the uni- or bigram, append a tuple to a list with the word and the NER tag
     wordlist = []  
@@ -97,7 +158,7 @@ def main():
         elif any(x in bigramdf for x in person):
             wordlist.append((first_item[bigramdeflist.index(bigramdf)], 'PER'))
             wordlist.append((second_item[bigramdeflist.index(bigramdf)], 'PER'))
-
     print(list(set(wordlist)))
+
 main()
 
