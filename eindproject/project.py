4218da2526f73603e1d8311932a8b22a72c543af
lennartschepers <lenschepers@gmail.com>
2018-06-18 20:56:49 +0200

    added proper shit

--- eindproject/project.py
+++ eindproject/project.py
@@ -33,6 +33,8 @@ def wikilinker(query):
     except wikipedia.DisambiguationError as e:
         return wikipedia.page(e.options[0]).url
 
+    except:
+        pass
 
 
     
@@ -67,17 +69,27 @@ def column(l, path):
         wg = wordgrouper(l)
         for row in posfile:
             #if stanford has an appriopriate tag, add it
-            x = [s for s in wg if row.split()[3] in s]
+            x = [s for s in wg if row.split()[3] in s.split(" ")]
+            #x = []
+            #for s in wg:
+                #if row.split()[3] in s.split(" "):
+                    #x.append(s)
+
 
             if l[n][1] != "O": 
                 columns = row.split()
                 columns.append(l[n][1])
 
                 if x:
-                    columns.append(wikilinker(x[0]))
-                    testfile.write(" ".join(columns))
-                    testfile.write("\n")
-                    n += 1
+                    try:
+
+                        columns.append(wikilinker(x[0]))
+                        testfile.write(" ".join(columns))
+                        testfile.write("\n")
+                        n += 1
+                    except TypeError:
+                        pass
+
                 else:
                     testfile.write(" ".join(columns))
                     testfile.write("\n")
@@ -91,29 +103,15 @@ def column(l, path):
 
         
            
-    #with open("testdir/" + folder + "/" + folder2 + "/en.tok.off.pos.test", "r+") as testfile:
-        #for row in testfile:
-#def linkadder(l, path):
-    #with open(path + "/en.tok.off.pos.test", "w+") as testfile:
-            #wg = wordgrouper(l)
-            #for row in testfile:
-                #print(row)
-                #x = [s for s in wg if row.split()[3] in s]
-                #columns = row.split()
-                #if x:
-                    #columns.append(wikipedia.page(x[0]).url)
-                    #testfile.write(" ".join(columns))
-                    #testfile.write("\n")
-
-
 
 
 def main():
     st = StanfordNERTagger('/home/lennart/Downloads/stanford-ner-2018-02-27/classifiers/english.conll.4class.distsim.crf.ser.gz', '/home/lennart/Downloads/stanford-ner-2018-02-27/stanford-ner.jar')
     #path = "/home/lennart/projecttextanalyse/eindproject/testdir/p05/d0580/"
-    for folder in os.listdir("/home/lennart/projecttextanalyse/eindproject/testdir/"):
-        for folder2 in os.listdir("/home/lennart/projecttextanalyse/eindproject/testdir/" + folder):
-            path = "/home/lennart/projecttextanalyse/eindproject/testdir/" + folder + "/" + folder2
-            column(tagger(path,st),folder2)
+    #for folder in os.listdir("/home/lennart/projecttextanalyse/eindproject/testdir/"):
+        #for folder2 in os.listdir("/home/lennart/projecttextanalyse/eindproject/testdir/" + folder):
+            #path = "/home/lennart/projecttextanalyse/eindproject/testdir/" + folder + "/" + folder2
+    path = "/home/lennart/projecttextanalyse/eindproject/testdir/p49/d0691"
+    column(tagger(path,st),path)
             #print(path)
 main()
