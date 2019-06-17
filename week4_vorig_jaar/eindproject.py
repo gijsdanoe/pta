a6446d1ed8bef95df7ff8a006306aa3be3ef5c31
lennartschepers <lenschepers@github.com>
2019-06-17 14:27:51 +0200

    natty bro

--- week4_vorig_jaar/eindproject.py
+++ week4_vorig_jaar/eindproject.py
@@ -14,10 +14,16 @@ def sfNERTagger(rawText):
     return list(parser.tag(rawText.split()))
 
 def sfNERWriter(POSFile, NERList):
-    '''Takes output of sfNERTagger() -->NERList, iters over the POSFile, if NERList[index][1] is meaningful: add the appropriate tag'''
-    for lineNumber, line in enumerate(POSFile):
-        if line.split()[3] == NERList[lineNumber][0]:
-            POSFile.write
+    '''Takes output of sfNERTagger() -->NERList, iters over the POSFile, if NERList[index][1] is meaningful: add the appropriate tag. create ENTFile and write every line'''
+    with open(POSFile, "r") as f1:
+        POSLines = f1.readlines()
+    with open(str(POSFile + ".ent"),"w") as f2:
+        for lineNumber, line in enumerate(POSLines):
+            line  = line.strip('\n')
+            if line.split()[3] == NERList[lineNumber][0]:
+                f2.write(str(line + " " + NERList[lineNumber][1] + '\n'))
+            else:
+                f2.write("error")
     # with open("posfile", "r") as posfile:
     #lines = posfile.readlines()
 #with open("posfile", "w") as posfile:
@@ -30,7 +36,7 @@ def getContinuousChunks(NERList):
     continuous_chunk = []
     current_chunk = []
 
-    for token, tag in tagged_sent:
+    for token, tag in NERList:
         if tag != "O":
             current_chunk.append((token, tag))
         else:
@@ -45,11 +51,16 @@ def getContinuousChunks(NERList):
 def main():
     with open('data/p51/d0060/en.raw') as f1:
         rawText = f1.read()
-        x = sfNERTagger(rawText)
+        NERRaw = sfNERTagger(rawText)
+        # x output example: [('out', 'O'),('two','Date")] etc
 
-    with open('data/p51/d0060/en.tok.off.pos') as f2:
-        for lineNumber, line in enumerate(f2):
-            if line.split()[3] == x[lineNumber][0]:
-                print('True')
+    #with open('data/p51/d0060/en.tok.off.pos') as f2:
+        #for lineNumber, line in enumerate(f2):
+            #if line.split()[3] == x[lineNumber][0]:
+                #print('True', NERRaw[lineNumber], f2.name + ".ent")
+
+    POSFile = 'data/p51/d0060/en.tok.off.pos'
+    #print(NERRaw)
+    sfNERWriter(POSFile, NERRaw)
 if __name__ == "__main__":
     main()
